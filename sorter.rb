c84da0991ef9038ef7d6b53b0f58a20bc9a9d41e
Davidm8624 <davidm8624@gmail.com>
2023-06-12 20:14:00 -0700

    done

---
+++ sorter.rb
@@ -0,0 +1,23 @@
+def bubble_sort(array)
+  n = array.length
+  sorted = false
+  
+  while !sorted
+    sorted = true
+    
+    (n-1).times do |i|
+      if array[i] > array[i+1]
+        array[i], array[i+1] = array[i+1], array[i]
+        sorted = false
+      end
+    end
+    
+    n -= 1
+  end
+  
+  array
+end
+
+unsorted = [1, 2, 3, 0, 1]
+sorted = bubble_sort(unsorted)
+print sorted
