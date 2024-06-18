473d8b2bc907133e4e8e59fe99a76654f12ba018
davidm8624 <davidm8624@gmail.com>
2024-06-17 17:31:03 -0700

    misc

--- sorter.rb
+++ sorter.rb
@@ -1,20 +1,20 @@
 def bubble_sort(array)
   n = array.length
   sorted = false
-  
+
   while !sorted
     sorted = true
-    
-    (n-1).times do |i|
-      if array[i] > array[i+1]
-        array[i], array[i+1] = array[i+1], array[i]
+
+    (n - 1).times do |i|
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
         sorted = false
       end
     end
-    
+
     n -= 1
   end
-  
+
   array
 end
 
